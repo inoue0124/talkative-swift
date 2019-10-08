e41f3d087bfd74c591065f012835aaa8549055e3
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2019-10-08 17:50:46 +0900

    Initial Commit

---
+++ talkative/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  talkative
+//
+//  Created by Yusuke Inoue on 2019/10/08.
+//  Copyright © 2019 Yusuke Inoue. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ talkative/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  talkative
+//
+//  Created by Yusuke Inoue on 2019/10/08.
+//  Copyright © 2019 Yusuke Inoue. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
